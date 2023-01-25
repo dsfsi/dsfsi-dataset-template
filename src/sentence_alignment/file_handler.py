bfcba80aade2d56b919c988979fcb20e38751036
lastrucci01 <u20430168@tuks.co.za>
2023-01-25 12:40:57 +0200

    :sparkles: Core code for sentence alignment done & documented

--- src/sentence_alignment/file_handler.py
+++ src/sentence_alignment/file_handler.py
@@ -1,46 +1,35 @@
-import os,re
+import os, re, pandas
+from pathlib import Path
 
-data_path = '../../data/processed/'
-languages = ['afr', 'eng', 'nbl', 'nso', 'sep', 'ssw', 'tsn', 'tso', 'ven', 'xho', 'zul']
+root_path = Path(os.path.abspath(__file__)).parent.parent.parent # vukuzenzele/
 
-def fetch_data_root_filepaths(): # -> list[str]
+raw_data_path = Path(root_path / 'data' / 'processed') #data/processed - raw in terms of sentence alignment
+token_data_path = Path(root_path  / 'data' / 'tokenised') #data/tokenised - the tokenised form
+output_data_path = Path(root_path / 'data' / 'sentence_align_output') #data/sentence_align_output - SA using encoders
+simp_out_data_path = Path(root_path / 'data' / 'simple_align_output')  #data/simple_align_output - plain SA, eg `hello` -> `hallo`
+
+languages = ['afr', 'eng', 'nbl', 'nso', 'sep', 'ssw', 'tsn', 'tso', 'ven', 'xho', 'zul'] # List of SA languages
+
+def fetch_data_edition_filepaths(): # -> list[str]
     """
     ### Compiles a list of file directories present in the /data/processed folder
+    #### Example filepath: `2020-01-ed1`
     """
-    root_paths = os.listdir(data_path) #list the directories in /data/processed
-    root_paths.remove('.gitkeep') 
-    return root_paths
+    edition_paths = os.listdir(raw_data_path) #list the directories in /data/processed
+    edition_paths.remove('.gitkeep') 
+    return edition_paths
 
-def fetch_lang_filepaths(lang): # -> dict[str, dict[str,list[str]]]
+def fetch_data_txt_filepaths(edition): # -> list[str]
     """
-    ### Creates a dict for each lang. 
-    #### Example (eng)
-    ```
-    {
-        'eng' : {
-            '2020-09-ed1' : [
-                '2020-09-ed1-vukuzenzele-eng-001.txt',  
-                '2020-09-ed1-vukuzenzele-eng-002.txt',  
-                '2020-09-ed1-vukuzenzele-eng-003.txt'  
-            ]
-        }
-    }
-    ```
+    ### Compiles a list of files present in the /data/processed/edition folder
+    #### Example filepath: `2020-02-ed1-vukuzenzele-afr-01.txt`
+    #### Params 
+        -   `edition` is the different editions of the magazine stored in data/processed
     """
-    lang_paths = {}
-    root_paths = fetch_data_root_filepaths()
-    for main_path in root_paths: # for each directory in data/processed
-        edition_paths = os.listdir('{}/{}'.format(data_path, main_path)) # list the files in data/processed/edition
-        for path in edition_paths:
-            match = re.search(lang, path) # look for files with lang tag
-            if match: # if lang found
-                if main_path in lang_paths.keys(): # if edition path already exists
-                    lang_paths[main_path].append(path) # append to existing list
-                else: lang_paths[main_path] = [path] # else create new list for edition path
+    txt_paths = os.listdir('{}/{}'.format(raw_data_path, edition)) #list the directories in /data/processed
+    return txt_paths
 
-    return {lang : lang_paths} # return new dict with lang tag key
-
-def compile_full_lang_dict(): # -> dict[str,dict[str,list[str]]]
+def build_filepaths_dictonary():
     """
     ### Creates a dict for every lang. 
     #### Example
@@ -63,24 +52,102 @@ def compile_full_lang_dict(): # -> dict[str,dict[str,list[str]]]
     }
     ```
     """
-    output_dict = {}
-    for lang in languages:
-        output_dict.update(fetch_lang_filepaths(lang))
-    return output_dict
+    filepaths_dictionary = {}
+    edition_paths = fetch_data_edition_filepaths()
+    for edition in edition_paths:
+        txt_paths = fetch_data_txt_filepaths(edition)
+        for txt in txt_paths:
+            lang = re.search('afr|eng|nso|nbl|sep|ssw|tsn|tso|ven|xho|zul', txt).group()
+            if lang not in filepaths_dictionary.keys():
+                filepaths_dictionary[lang] = {edition : [txt]}
+            elif edition not in filepaths_dictionary[lang].keys():
+                filepaths_dictionary[lang][edition] = [txt]
+            else: 
+                filepaths_dictionary[lang][edition].append(txt)
 
-def read_file(edition_path, txt_path): # -> str
+    return filepaths_dictionary
+
+def read_file_as_string(edition_path, txt_path): # -> str
     """
-    ### Reads file as a single string.
+    ### Reads file as a single string. 
+
+    Generally used to feed a single line text to the tokeniser
     """
-    return open(data_path + edition_path + '/' + txt_path, 'r').read() 
+    file_path = Path(raw_data_path / edition_path / txt_path)
+    return open(file_path, 'r').read() 
+
+def read_file_as_array(edition_path, txt_path): # -> str
+    """
+    ### Reads file as an array.
+
+    Generally used to read tokenised data for further processing
+    """
+    file_path = Path(token_data_path / edition_path / txt_path)
+    return open(file_path, 'r').readlines() 
 
 
 def write_tokens_to_txt(edition_path, txt_path, tokens): # -> None
-    if not os.path.exists('../../data/tokenised/{}'.format(edition_path)):
-        os.makedirs('../../data/tokenised/{}'.format(edition_path))
-    new = open('../../data/tokenised/{}/{}'.format(edition_path, txt_path),'w')
-    for token in tokens:
-        new.write(token)
-        if not tokens[len(tokens)-1]==token:
-            new.write('\n')
+    """
+    ### Writes a token array to a .txt in the data/tokenised folder
+    #### Params:
+        -   edition_path: path_name representing the edition to create a new dir for the tokens
+        -   txt_path: path_name to the sentence tokens to create a new txt file
+        -   tokens: list of sentence tokens to be written to file
+    """
+    if not os.path.exists(Path(token_data_path / edition_path)): #if data/tokenised/edition does not exist
+        os.makedirs((Path(token_data_path / edition_path))) # create it
 
+    new = open((Path(token_data_path / edition_path / txt_path)),'w') # open a txt file in write mode
+    for token in tokens: #for each tokem
+        new.write(token) # write to new file
+        if not tokens[len(tokens)-1]==token: # the tokens seem to be missing newlines so if token is not the last token,
+            new.write('\n')  # write a newline
+
+def append_to_final_csv(src_lang, src_sentences, src_vector, tgt_lang, tgt_sentences ,tgt_vector, sim_scores):
+    """
+    ### Appends to the ML aligned lang pairs .csv - creates it if it doesn't exist
+    #### Params
+        -   src_lang: source lang (str)
+        -   src_sentences: source sentence tokens (list)
+        -   src_vector: source sentence vectors (list)
+        -   tgt_lang: target lang (str)
+        -   tgt_sentences: target sentence tokens (list)
+        -   tgt_vector: target sentence vectors (list)
+        -   sim_scores: confidence scores between the pairing (list)
+    """
+    data = {
+        src_lang : src_sentences,
+        tgt_lang : tgt_sentences,
+        '{}_vector'.format(src_lang) : src_vector,
+        '{}_vector'.format(tgt_lang) : tgt_vector,
+        'cosine_score' : sim_scores
+    } # build a dictonary of the info
+
+    df = pandas.DataFrame(data) #turn into a pandas DF
+
+    csv_path = Path('aligned_{}_{}.csv'.format(src_lang, tgt_lang)) # Path of lang aligned csv
+    if not os.path.exists(output_data_path): # if data/sentence_alignment doesnt exist
+        os.makedirs(output_data_path) # create it 
+
+    if os.path.exists(Path(output_data_path / csv_path)): # if .csv does exist
+        df.to_csv(Path(output_data_path / csv_path), mode='a',header=False, index=False) # append with no headers
+    else: 
+        df.to_csv(Path(output_data_path / csv_path), mode='w',header=True, index=False) # create with headers
+
+
+def append_to_simple_csv(src_lang, src_sentences, tgt_lang, tgt_sentences):
+
+    data = {
+        src_lang : src_sentences,
+        tgt_lang : tgt_sentences,
+    }
+    df = pandas.DataFrame(data)
+
+    csv_path = Path('aligned_{}_{}.csv'.format(src_lang, tgt_lang))
+    if not os.path.exists(simp_out_data_path):
+        os.makedirs(simp_out_data_path)
+
+    if os.path.exists(Path(output_data_path / csv_path)):
+        df.to_csv(Path(simp_out_data_path / csv_path), mode='a',header=False, index=False)
+    else: 
+        df.to_csv(Path(simp_out_data_path / csv_path), mode='w',header=True, index=False)
--- src/sentence_alignment/sentence_embedding.py
+++ src/sentence_alignment/sentence_embedding.py
@@ -1,12 +1,40 @@
-import subprocess
+import subprocess, os, numpy
+from pathlib import Path
 
+from laser_config import laser_path
+from file_handler import root_path
 
-DATA_PATH = "../../data/tokenised/"
+embed_data_path = Path(root_path / 'data' / 'embed')
 
+def encode_sentences(edition_path, txt_path, lang_model):
+    """
+    ### Calls LASER task to encode a txt_file and outputs a raw_file that can be decoded into a sentece vector
+    #### Example
+    `hello` -> raw format
+    #### Params:
+        -   edition_path: the path_name to the dir for this edition to be created in data/embed (str)
+        -   txt_path: the path_name for the raw output to be written to (str)
+        -   lang_model: the lang model to be used in the encoding (str)
+    """
+    if not os.path.exists(Path(embed_data_path / edition_path)): # if data/embed/edition doesn't exist
+        os.makedirs(Path(embed_data_path / edition_path)) # make it 
+    output_path = Path(embed_data_path / edition_path / txt_path) # the path to output
 
-def embed_sentences(txt_path, output_path, lang_model):
-    command = f'bash ./LASER/tasks/embed/embed.sh '
-    command += "{} {} {}".format(f'{DATA_PATH}{txt_path}', output_path, lang_model)
-    subprocess.run(command, shell=True)
+    command = f'bash {laser_path}/tasks/embed/embed.sh' # the command without params
+    command += "{} {} {}".format(Path(embed_data_path / edition_path / txt_path), output_path, lang_model) # add params which are source text, output path & lang model
+    subprocess.run(command, shell=True) # run the bash command using the shell
     
-    
+def decode_sentences(edition_path, txt_path):
+    """
+    ### Decodes the raw output into a sentence vector - the vector stores in the sentence meaning in computer terms
+    #### Example
+    raw format -> `[3, 4, ... ,5, 6]`
+     #### Params:
+        -   edition_path: the path_name to the dir for this edition (str)
+        -   txt_path: the path_name for the raw output to be decoded (str)
+    """
+    dim = 1024 # dimensions
+    path = Path(embed_data_path / edition_path / txt_path) # path to file
+    vector_list = numpy.fromfile(path, dtype=numpy.float32, count=-1) # fancy numpy coding idk                                                               
+    vector_list.resize(vector_list.shape[0] // dim, dim)  # more fancy
+    return vector_list # return list of vectors
