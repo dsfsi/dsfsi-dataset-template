48458bdb6fed845e2a4020f5eaf7d7213066d005
lastrucci01 <u20430168@tuks.co.za>
2023-01-26 12:08:17 +0200

    :sparkles: Action works

--- src/sentence_alignment/main.py
+++ src/sentence_alignment/main.py
@@ -19,65 +19,53 @@ lang_mappings = {
 
 if __name__ == "__main__":
 
-    # setup laser
-    laser_config.set_environ_var()
-    laser_config.setup_laser()
-    laser_config.download_laser_models(lang_mappings)
-    laser_config.download_tokeniser()
-    
+   
     
     # create directories dictionary
     filepaths_dictionary = file_handler.build_filepaths_dictonary()
     last_date = file_handler.extract_latest_edition()
     edition_keys = file_handler.fetch_data_edition_filepaths(last_date);
 
-    # perform tokenisation
-    for edition in edition_keys:
-        for lang in languages:
-            if edition not in filepaths_dictionary[lang]:
-                continue
-            for txt in filepaths_dictionary[lang][edition]:
-                text = file_handler.read_file_as_string(edition, txt)
-                tokens = sentence_tokenisation.tokenise(text)
-                file_handler.write_tokens_to_txt(edition, txt, tokens)
+    if len(edition_keys) > 0:
+         # setup laser
+        laser_config.set_environ_var()
+        laser_config.setup_laser()
+        laser_config.download_laser_models(lang_mappings)
+        laser_config.download_tokeniser()
 
-    # perform LASER encoding
-    for edition in edition_keys:
-        for lang in lang_mappings.keys():
-            if edition not in filepaths_dictionary[lang]:
-                continue
-            for txt in filepaths_dictionary[lang][edition]:
-                sentence_embedding.encode_sentences(edition, txt, lang_mappings[lang])
+        # perform tokenisation
+        for edition in edition_keys:
+            for lang in languages:
+                if edition not in filepaths_dictionary[lang]:
+                    continue
+                for txt in filepaths_dictionary[lang][edition]:
+                    text = file_handler.read_file_as_string(edition, txt)
+                    tokens = sentence_tokenisation.tokenise(text)
+                    file_handler.write_tokens_to_txt(edition, txt, tokens)
 
-    # perform SA on LASER encoded sentences
-    for first_lang in lang_mappings.keys():
-        for sec_lang in lang_mappings.keys():
-            if first_lang != sec_lang:
-                for edition in edition_keys:
-                    sentence_alignment.two_lang_alignment(first_lang, sec_lang, edition)
+        # perform LASER encoding
+        # for edition in edition_keys:
+        #     for lang in lang_mappings.keys():
+        #         if edition not in filepaths_dictionary[lang]:
+        #             continue
+        #         for txt in filepaths_dictionary[lang][edition]:
+        #             sentence_embedding.encode_sentences(edition, txt, lang_mappings[lang])
 
-
-    # # perform basic sentece alignment on tokenised sentences
-    for first_lang in languages:
-        for sec_lang in languages:
-            if first_lang != sec_lang:
-                for edition in edition_keys:
-                    sentence_alignment.simple_langs_alignment(first_lang, sec_lang, edition)
-
-    # write last edition reviewed to file so as not to review in future
-    file_handler.write_latest_edition(edition_keys[len(edition_keys)-1])
-    
+        # perform SA on LASER encoded sentences
+        for first_lang in lang_mappings.keys():
+            for sec_lang in lang_mappings.keys():
+                if first_lang != sec_lang:
+                    for edition in edition_keys:
+                        sentence_alignment.two_lang_alignment(first_lang, sec_lang, edition)
 
 
-    # for directory in root_dirs:
-    #     listdir = os.listdir("../../data/tokenised/{}".format(directory))
-    #     for text_txt in listdir:
-    #         match = re.search('nso|ssw|tsn|tso|xho|zul', text_txt)
-    #         output = text_txt
-    #         re.sub("\.txt","\.raw", output)
-    #         if match:
-    #             matched = match.group()
-    #             if not os.path.exists('../../data/embed/{}'.format(directory)):
-    #                 os.makedirs('../../data/embed/{}'.format(directory))
-    #             sentence_embedding.embed_sentences(f'{directory}/{text_txt}', "../../data/embed/{}/{}".format(directory,output),lang_mappings[matched])
+        # # perform basic sentece alignment on tokenised sentences
+        for first_lang in languages:
+            for sec_lang in languages:
+                if first_lang != sec_lang:
+                    for edition in edition_keys:
+                        sentence_alignment.simple_langs_alignment(first_lang, sec_lang, edition)
+
+        # write last edition reviewed to file so as not to review in future
+        file_handler.write_latest_edition(edition_keys[len(edition_keys)-1])
 
