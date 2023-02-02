52f98324a34c70f88448ac6d2b235af660be6959
Peace Azugo <peaceazugo76@gmail.com>
2023-02-02 09:21:23 +0200

    Updates to sent align script

--- src/sentence_alignment/main.py
+++ src/sentence_alignment/main.py
@@ -1,4 +1,5 @@
-import laser_config, file_handler, sentence_tokenisation, sentence_embedding, sentence_alignment, os, re, pathlib
+import laser_config, file_handler, sentence_tokenisation, sentence_embedding, sentence_alignment
+from itertools import combinations
 
 languages = ['afr', 'eng', 'nbl', 'nso', 'sep', 'ssw', 'tsn', 'tso', 'ven', 'xho', 'zul']
 
@@ -6,13 +7,13 @@ lang_mappings = {
                     'afr' : '',
                     'eng' : '',
                     'nbl' : '',
-                    # 'nso' : 'sot_Latn',
+                    'nso' : 'sot_Latn',
                     'sep' : '',
-                    # 'ssw' : 'ssw_Latn',
-                    # 'tsn' : 'tsn_Latn',
-                    # 'tso' : 'tso_Latn',
+                    'ssw' : 'ssw_Latn',
+                    'tsn' : 'tsn_Latn',
+                    'tso' : 'tso_Latn',
                     'ven' : '',
-                    # 'xho' : 'xho_Latn',
+                    'xho' : 'xho_Latn',
                     'zul' : 'zul_Latn',
                 } 
 
@@ -22,7 +23,9 @@ if __name__ == "__main__":
     # create directories dictionary
     filepaths_dictionary = file_handler.build_filepaths_dictonary()
     last_date = file_handler.extract_latest_edition()
-    edition_keys = file_handler.fetch_data_edition_filepaths(last_date);
+    edition_keys = file_handler.fetch_data_edition_filepaths(last_date)
+    language_pairs = list(combinations(languages, 2))
+
 
     if len(edition_keys) > 0:
         print('performing sentece aligment on new edtions...')
@@ -43,27 +46,23 @@ if __name__ == "__main__":
                     file_handler.write_tokens_to_txt(edition, txt, tokens)
 
         # perform LASER encoding
-        for edition in edition_keys:
-            for lang in lang_mappings.keys():
-                if edition not in filepaths_dictionary[lang]:
-                    continue
-                for txt in filepaths_dictionary[lang][edition]:
-                    sentence_embedding.encode_sentences(edition, txt, lang_mappings[lang])
+        # for edition in edition_keys:
+        #     for lang in lang_mappings.keys():
+        #         if edition not in filepaths_dictionary[lang]:
+        #             continue
+        #         for txt in filepaths_dictionary[lang][edition]:
+        #             sentence_embedding.encode_sentences(edition, txt, lang_mappings[lang])
 
         # perform SA on LASER encoded sentences
-        for first_lang in lang_mappings.keys():
-            for sec_lang in lang_mappings.keys():
-                if first_lang != sec_lang:
-                    for edition in edition_keys:
-                        sentence_alignment.two_lang_alignment(first_lang, sec_lang, edition)
+        for (first_lang, sec_lang) in language_pairs:
+            for edition in edition_keys:
+                sentence_alignment.two_lang_alignment(first_lang, sec_lang, edition)
 
 
         # # perform basic sentece alignment on tokenised sentences
-        # for first_lang in languages:
-        #     for sec_lang in languages:
-        #         if first_lang != sec_lang:
-        #             for edition in edition_keys:
-        #                 sentence_alignment.simple_langs_alignment(first_lang, sec_lang, edition)
+        for (first_lang, sec_lang) in language_pairs:
+            for edition in edition_keys:
+                sentence_alignment.simple_langs_alignment(first_lang, sec_lang, edition)
 
         # write last edition reviewed to file so as not to review in future
         file_handler.write_latest_edition(edition_keys[len(edition_keys)-1])
